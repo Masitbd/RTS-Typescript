339ce954e517f678a540963e8936a3db0ead0550
Murad Sarker <sarker24@gmail.com>
2021-06-01 00:22:48 +0200

    Event handler

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import EventComponent from "./events/Event<<component";
 import UserSearch from "./State/UserSearch";
 
 
 const App = () => {
     return <div>
-        <UserSearch />
+        <EventComponent />
 
     </div>
 }
