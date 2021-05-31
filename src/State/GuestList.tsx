b59592c37f29b5f7d5c0bad27f20f19a60531864
Murad Sarker <sarker24@gmail.com>
2021-05-31 22:12:10 +0200

    Add list using state

--- src/State/GuestList.tsx
+++ src/State/GuestList.tsx
@@ -3,10 +3,29 @@ import { useState } from "react";
 const GuestList:React.FC = () =>{
 
     const [name, setName] = useState('');
+    const [guests, setGuests] = useState<string[]>([]);
+
+    const onClick = () =>{
+
+        setName('');
+        setGuests([...guests, name])
+
+    }
     return <div>
           <h3>Guest list</h3>
-            <input type="text"/>
-            <button>Add Guest</button>
+          <ul>
+              {guests.map((gust) => {
+               return   <li key={gust}>            
+                      {gust}
+                 </li>
+              })}
+          </ul>
+            <input 
+            type="text"
+            value={name}
+            onChange = {(e) => setName(e.target.value)}
+            />
+            <button onClick={onClick}>Add Guest</button>
         </div>
 }
 
